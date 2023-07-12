33870d6443fead321a6010f5cf3eae46e9247618
Caio Francelino <caio.francelinosena@gmail.com>
2023-07-12 03:20:11 -0400

    estrutura de controle interativa

---
+++ 03 - control structure/interaction/for.rb
@@ -0,0 +1,6 @@
+# leia uma lista (array) de elementos - linguagens de programação
+linguagens = ['Ruby', 'Go', 'Java', 'C++']
+
+for linguagem in linguagens do
+    puts(linguagem)
+end
---
+++ 03 - control structure/interaction/while.rb
@@ -0,0 +1,8 @@
+# comparar um numero com uma condição
+
+numero = 1
+
+while numero <= 100 do
+    puts(numero)
+    numero+=1
+end
