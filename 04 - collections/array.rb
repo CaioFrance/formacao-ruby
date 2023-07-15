57e5558529041cfd66ccd76b353adbbbe2f26be3
Caio Francelino Sena Santos <Caio@Caios-iMac.local>
2023-07-14 21:22:20 -0700

    arrays

---
+++ 04 - collections/array.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# lista de livros
+
+livros = []
+
+livros.push('Memórias Póstumas de Brás Cubas')
+livros.push('Iracema')
+livros.insert(0, 'Vidas Secas')
+
+p(livros)
+
+puts(livros[2])
+
+puts(livros.first)
